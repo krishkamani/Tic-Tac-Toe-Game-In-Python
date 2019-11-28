8c09034f1d862ed0e2ec72a1505e95af942b3115
krishkamani <52067673+krishkamani@users.noreply.github.com>
2019-11-28 17:08:55 +0530

    Update Tic_Tac_Toe_Game.py

--- Tic_Tac_Toe_Game.py
+++ Tic_Tac_Toe_Game.py
@@ -41,9 +41,44 @@ bu9=ttk.Button(root,text=' ')
 bu9.grid(row=2,column=2,sticky='snew',ipadx=40,ipady=40)
 bu9.config(command=lambda: ButtonClick(9))
 
+playerturn=ttk.Label(root,text="   Player 1 turn!  ")
+playerturn.grid(row=3,column=0,sticky='snew',ipadx=40,ipady=40)
+
+playerdetails=ttk.Label(root,text="    Player 1 is X\n\n    Player 2 is O")
+playerdetails.grid(row=3,column=2,sticky='snew',ipadx=40,ipady=40)
+
+res=ttk.Button(root,text='Restart')
+res.grid(row=3,column=1,sticky='snew',ipadx=40,ipady=40)
+res.config(command=lambda: restartbutton())
+
 a=1
 b=0
-
+c=0
+def restartbutton():
+    global a,b,c
+    a=1
+    b=0
+    c=0
+    playerturn['text']="   Player 1 turn!   "
+    bu1['text']=' '
+    bu2['text']=' '
+    bu3['text']=' '
+    bu4['text']=' '
+    bu5['text']=' '
+    bu6['text']=' '
+    bu7['text']=' '
+    bu8['text']=' '
+    bu9['text']=' '
+    bu1.state(['!disabled'])
+    bu2.state(['!disabled'])
+    bu3.state(['!disabled'])
+    bu4.state(['!disabled'])
+    bu5.state(['!disabled'])
+    bu6.state(['!disabled'])
+    bu7.state(['!disabled'])
+    bu8.state(['!disabled'])
+    bu9.state(['!disabled'])
+    
 #after getting result(win or loss or draw) disable button
 def disableButton():
     bu1.state(['disabled'])
@@ -58,8 +93,9 @@ def disableButton():
 
 
 def ButtonClick(id):
-    global a,b
+    global a,b,c
     print("ID:{}".format(id))
+
     #for player 1 turn
     if id==1 and bu1['text']==' ' and a==1:
         bu1['text']="X"
@@ -134,7 +170,7 @@ def ButtonClick(id):
         bu9['text']="O"
         a=1
         b+=1
-    
+        
     #checking for winner   
     if( bu1['text']=='X' and bu2['text']=='X' and bu3['text']=='X' or
         bu4['text']=='X' and bu5['text']=='X' and bu6['text']=='X' or
@@ -145,6 +181,7 @@ def ButtonClick(id):
         bu1['text']=='X' and bu5['text']=='X' and bu9['text']=='X' or
         bu3['text']=='X' and bu5['text']=='X' and bu7['text']=='X'):
             disableButton()
+            c=1
             tkinter.messagebox.showinfo("Tic Tac Toe","Winner is player 1")
     elif( bu1['text']=='O' and bu2['text']=='O' and bu3['text']=='O' or
         bu4['text']=='O' and bu5['text']=='O' and bu6['text']=='O' or
@@ -155,10 +192,17 @@ def ButtonClick(id):
         bu1['text']=='O' and bu5['text']=='O' and bu9['text']=='O' or
         bu3['text']=='O' and bu5['text']=='O' and bu7['text']=='O'):
             disableButton()
+            c=1
             tkinter.messagebox.showinfo("Tic Tac Toe","Winner is player 2")
     elif b==9:
             disableButton()
+            c=1
             tkinter.messagebox.showinfo("Tic Tac Toe","Match is Draw.")
+
+    if a==1 and c==0:
+        playerturn['text']="   Player 1 turn!   "
+    elif a==0 and c==0:
+        playerturn['text']="   Player 2 turn!   "
             
 root.mainloop()
 
